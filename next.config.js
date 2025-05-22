5df3cb5c8c99e42c2217c845d9578a938fd27c97
Sam Bonomo <sam.bonomo@bonomoinvestments.com>
2025-05-22 12:46:12 -0400

    u13.5

--- next.config.js
+++ next.config.js
@@ -3,4 +3,11 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /\.ts$/,
+      exclude: /functions/,
+    });
+    return config;
+  },
 };
